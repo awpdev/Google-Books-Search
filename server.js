9fc204722bd33ce43aba24d0186ee405d1104f4f
Andrew Park <apark999@gmail.com>
2021-02-25 14:13:03 -0800

    modified server js

--- server.js
+++ server.js
@@ -16,7 +16,13 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebookssearch");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}).then(() => console.log('Database connected successfully'))
+  .catch(err => console.log(err));
 
 // Start the API server
 app.listen(PORT, function() {
